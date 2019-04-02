8b107311e98dae9d56f187920ba039d061be6597
Edgardo Jose Bulaong <ejbulaong@yahoo.com>
2019-04-02 10:48:09 -0500

    fixed account controller views

---
+++ Bug Tracker/Models/ViewModels/CreateProjectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bug_Tracker.Models.ViewModels
+{
+    public class CreateProjectViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+    }
+}
