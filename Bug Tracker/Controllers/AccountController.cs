95d4450a2897f33ad1283af2613cfeb10da207b3
Edgardo Jose Bulaong <ejbulaong@yahoo.com>
2019-04-02 09:53:19 -0500

    setting Submitter as a default role

--- Bug Tracker/Controllers/AccountController.cs
+++ Bug Tracker/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Bug_Tracker.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Bug_Tracker.Controllers
 {
@@ -22,7 +23,7 @@ namespace Bug_Tracker.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -34,9 +35,9 @@ namespace Bug_Tracker.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -120,7 +121,7 @@ namespace Bug_Tracker.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,12 +152,30 @@ namespace Bug_Tracker.Controllers
         {
             if (ModelState.IsValid)
             {
+                ApplicationDbContext context = new ApplicationDbContext();
+
+                var roleManager =
+                    new RoleManager<IdentityRole>(
+                        new RoleStore<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>(context));
+
+                var userManager =
+                    new UserManager<ApplicationUser>(
+                            new UserStore<ApplicationUser>(context));
+
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+
+                if (!context.Roles.Any(p => p.Name == "Submitter"))
+                {
+                    var moderatorRole = new IdentityRole("Submitter");
+                    roleManager.Create(moderatorRole);
+                }
+
                 var result = await UserManager.CreateAsync(user, model.Password);
+                userManager.AddToRole(user.Id, "Submitter");
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
