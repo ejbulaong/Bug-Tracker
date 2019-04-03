c79ff5b844662b391af397c95bc7e47e39680a63
Edgardo Jose Bulaong <ejbulaong@yahoo.com>
2019-04-03 13:29:05 -0500

    displaying user roles

---
+++ Bug Tracker/Controllers/BugController.cs
@@ -0,0 +1,60 @@
+﻿using Bug_Tracker.Models;
+using Bug_Tracker.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Bug_Tracker.Controllers
+{
+    public class BugController : Controller
+    {
+        private ApplicationDbContext DbContext;
+
+        public BugController()
+        {
+            DbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ActionResult ManageUsers()
+        {
+            //UserManager, used to manage users
+            var userManager =
+                new UserManager<ApplicationUser>(
+                        new UserStore<ApplicationUser>(DbContext));
+
+            var model = new List<ManageUsersViewModel>();
+
+            foreach (var user in DbContext.Users)
+            {
+                model.Add(
+                    new ManageUsersViewModel
+                    {
+                        Name = user.Name,
+                        UserName = user.Email
+                    });
+            }
+
+            foreach (var m in model)
+            {
+                var userId = (from u in DbContext.Users
+                              where u.UserName == m.UserName
+                              select u.Id).FirstOrDefault();
+
+                m.Role = userManager.GetRoles(userId);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Bug Tracker/Models/ViewModels/ManageUsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bug_Tracker.Models.ViewModels
+{
+    public class ManageUsersViewModel
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public IList<string> Role { get; set; }
+    }
+}
